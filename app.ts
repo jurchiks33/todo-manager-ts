29a174a10b72aa8489bc76af0ac67db681037d23
jurchiks33 <jurisslisans@gmail.com>
2023-10-21 13:40:14 +0200

    working on JS on eventListeners

---
+++ app.ts
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const addButton = document.getElementById('addTodo') as HTMLButtonElement;
+    const inputField = document.getElementById('todoInput') as HTMLInputElement;
+    const todoList = document.getElementById('todoList') as HTMLUListElement;
+
+    function addTodo() {
+        const todoText = inputField.value;
+    }
+}
+)
